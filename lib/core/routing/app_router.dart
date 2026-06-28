9f006e9d856369eda6ffb393a56f862329f9871c
pik15 <opikjk9@gmail.com>
2026-06-28 09:06:31 +0700

    error 1

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,8 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import '../di/injection.dart'; // Import locator GetIt kamu
+import '../../features/home/presentation/bloc/crypto_cubit.dart'; // Import Cubit
 import '../../features/home/presentation/pages/home_page.dart';
 import '../../features/home/presentation/pages/detail_page.dart';
 import '../../features/profile/presentation/pages/profile_page.dart';
-import '../../features/home/presentation/pages/bookmark_page.dart'; 
+import '../../features/home/presentation/pages/bookmark_page.dart';
 import '../../features/home/data/models/crypto_api_model.dart';
 
 class AppRouter {
@@ -11,7 +14,11 @@ class AppRouter {
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const HomePage(),
+        // FIX: Bungkus HomePage dengan BlocProvider agar CryptoCubit tersedia di dalam BuildContext
+        builder: (context, state) => BlocProvider<CryptoCubit>(
+          create: (context) => locator<CryptoCubit>(),
+          child: const HomePage(),
+        ),
       ),
       GoRoute(
         path: '/detail',
@@ -24,7 +31,6 @@ class AppRouter {
         path: '/profile',
         builder: (context, state) => const ProfilePage(),
       ),
-      // Tambahkan rute untuk Halaman Bookmark Offline Vault
       GoRoute(
         path: '/bookmark',
         builder: (context, state) => const BookmarkPage(),
